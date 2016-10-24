edd700b91a605ee02053b918c3ae25bca76fef8d
abraham polishchuk <apolishc@gmail.com>
2016-10-24 21:46:41 +0100

    brute force

--- longest-subsequence/lib/longest_subsequence.rb
+++ longest-subsequence/lib/longest_subsequence.rb
@@ -1,16 +1,33 @@
 module LongestSubsequence
-
-	def self.find(first, second)
-    first_ary = first.chars
-    (0..first_ary.length-1).reverse_each do |start_chr|
-      (0..first_ary.length- start_chr -1).reverse_each do |end_chr|
-        current = first_ary.slice(start_chr, end_chr)
-         second.each do |second_chr|
-           if  
-         end
-      end
+  def self.find(first, second)
+    if ([first, second].include? nil)
+      return nil
     end
-
-	end
-
+    if (first == second)
+      return first
+    end
+    if (first[-1] == second[-1])
+      return ((LongestSubsequence.find(first[0..-2], second[0..-2]) || "") + first[-1])
+    end
+    temp_first = first
+    temp_second = second.chars
+    longest_substring = ""
+    current_substring = ""
+    # temp_first.inject .....
+    (0..(temp_first.length-1)).each do |outer_index|
+      first.chars.each.with_index do |char, index|
+        if (temp_second.include? char)
+          current_substring += char
+          temp_second = temp_second[(temp_second.index(char))..-1]
+        end
+        if (index == (temp_first.length-1) || temp_second == nil)
+          longest_substring = (current_substring.length > longest_substring.length) ? current_substring : longest_substring
+          current_substring = ""
+          temp_second = second.chars
+        end
+      end
+      first = first[1..-1]
+    end
+    (longest_substring == "") ? nil : longest_substring
+  end
 end
--- longest-subsequence/spec/longest_subsequence_spec.rb
+++ longest-subsequence/spec/longest_subsequence_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe LongestSubsequence, type: :module do
       longest = LongestSubsequence.find(nil, nil)
       expect(longest).to eq nil
     end
+
+    it "should result in nil when either string nil" do
+      longest1 = LongestSubsequence.find(nil, "foo")
+      longest2 = LongestSubsequence.find("foo", nil)
+      expect(longest1).to eq nil
+      expect(longest2).to eq nil
+    end
     
     it "should result in nil when given no common subsequences" do
       longest = LongestSubsequence.find("a", "b")
@@ -17,6 +24,20 @@ RSpec.describe LongestSubsequence, type: :module do
       longest = LongestSubsequence.find("ABCDAF", "ACBCF")
       expect(longest.chars.sort.join).to eq "ABCF"
     end
-    
+
+    it "should returb substring" do
+      longest = LongestSubsequence.find("ABCDE", "ADBCD")
+      expect(longest.chars.sort.join).to eq("ABCD")
+    end
+
+    it "should return the longest possible subsequence" do
+      longest = LongestSubsequence.find("ABCDAFGHIJK", "ABFGHIJK")
+      expect(longest.chars.sort.join).to eq "ABFGHIJK"
+    end
+
+    it "when the only commonality is at the end of the strings" do
+      longest = LongestSubsequence.find("ZXYJKLMNOP", "ABCDEFGMNOP")
+      expect(longest.chars.sort.join).to eq "MNOP"
+    end
   end
 end
